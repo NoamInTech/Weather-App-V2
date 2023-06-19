6bcab62b180265c8aa3fba0200c71839f15f39f2
NoamInTech <noemie.pivin@gmail.com>
2023-06-19 23:52:58 +0100

    Added the GitHub repo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        {" "}
+        This project was coded by NoamInTech and is{" "}
+        <a
+          href="https://joyful-muffin-b2714d.netlify.app/"
+          target="_blank"
+        ></a>{" "}
+        open-sourced on GitHub.
+      </footer>
+    </div>
+  );
 }
