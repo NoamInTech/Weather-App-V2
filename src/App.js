2afa33a6e695f626a574f4829f0492d7885f5579
NoamInTech <noemie.pivin@gmail.com>
2023-06-19 23:56:21 +0100

    Imported React on App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <footer>
         {" "}
         This project was coded by NoamInTech and is{" "}
